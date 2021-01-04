56f2cd564ca32bcf5c1ee45d9b0685b6f91dfe82
Carolin Nygren <nygren.c@gmail.com>
2021-01-04 16:32:12 +0100

    Apicontroller bara skapad. TextArea till presentation.

---
+++ Datingsida/Controllers/Api/MessageApiController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Datingsida.DataAccess;
+using Datingsida.Models;
+
+namespace Datingsida.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageApiController : ControllerBase
+    {
+        private readonly DatingDbContext _context;
+
+        public MessageApiController(DatingDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/MessageApi
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MessageModel>>> GetMessageModel()
+        {
+            return await _context.MessageModel.ToListAsync();
+        }
+
+        // GET: api/MessageApi/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<MessageModel>> GetMessageModel(int id)
+        {
+            var messageModel = await _context.MessageModel.FindAsync(id);
+
+            if (messageModel == null)
+            {
+                return NotFound();
+            }
+
+            return messageModel;
+        }
+
+        // PUT: api/MessageApi/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMessageModel(int id, MessageModel messageModel)
+        {
+            if (id != messageModel.MessageId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(messageModel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MessageModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/MessageApi
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<MessageModel>> PostMessageModel(MessageModel messageModel)
+        {
+            _context.MessageModel.Add(messageModel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMessageModel", new { id = messageModel.MessageId }, messageModel);
+        }
+
+        // DELETE: api/MessageApi/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMessageModel(int id)
+        {
+            var messageModel = await _context.MessageModel.FindAsync(id);
+            if (messageModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.MessageModel.Remove(messageModel);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MessageModelExists(int id)
+        {
+            return _context.MessageModel.Any(e => e.MessageId == id);
+        }
+    }
+}
---
+++ Datingsida/Data/Migrations/20210104135925_meddelanden.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Datingsida.Migrations.DatingDb
+{
+    public partial class meddelanden : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MessageModel",
+                columns: table => new
+                {
+                    MessageId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Subjekt = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MessageText = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ToId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FromId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateOfPost = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MessageModel", x => x.MessageId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MessageModel");
+        }
+    }
+}
